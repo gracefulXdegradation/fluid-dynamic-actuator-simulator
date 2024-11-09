aedf1f867d15d55c4fbbf531a4d704e7e3ef2e54
Sasha Grynevych <alexandr.grinevich@gmail.com>
2024-11-09 17:21:26 +0100

    feat: added eigen

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <Eigen/Dense>
 #include "TLE.h"
 
+using namespace Eigen;
 using namespace std;
 
 int main()
@@ -15,10 +16,26 @@ int main()
         std::cout << "Satellite Number: " << tle.getSatelliteNumber() << std::endl;
         std::cout << "Epoch Year: " << tle.getEpochYear() << std::endl;
         std::cout << "Mean Motion: " << tle.getMeanMotion() << std::endl;
+
+        MatrixXd A(2, 3);
+        A << 1, 2, 3,
+            4, 5, 6;
+
+        MatrixXd B(3, 2);
+        B << 7, 8,
+            9, 10,
+            11, 12;
+
+        MatrixXd result = A * B;
+        MatrixXd transposedResult = result.transpose();
+
+        cout << "Result of (A * B)^T:\n"
+             << transposedResult << endl;
     }
     catch (const std::exception &e)
     {
         std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
     }
     return 0;
 }
