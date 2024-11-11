45311f4f5cab24e927e3d93c269c73cc714805e4
Sasha Grynevych <alexandr.grinevich@gmail.com>
2024-11-11 23:04:53 +0100

    feat: nadir_frame halfway

--- simulator/src/main.cpp
+++ simulator/src/main.cpp
@@ -10,6 +10,44 @@
 using namespace Eigen;
 using namespace std;
 
+Eigen::MatrixXd cross(const Eigen::MatrixXd &a, const Eigen::MatrixXd &b)
+{
+    if (a.rows() != 3 || b.rows() != 3 || a.cols() != b.cols())
+    {
+        std::cerr << "Error: Matrices must be 3xN in size and have the same number of columns.\n";
+        exit(1); // Exit if the matrices have incompatible sizes
+    }
+
+    // Initialize the result matrix with the same number of rows and columns
+    Eigen::MatrixXd x(3, a.cols());
+
+    // Loop over each column and compute the cross product
+    for (int i = 0; i < a.cols(); ++i)
+    {
+        // Extract the 3D vectors from the matrices (column i)
+        Eigen::Vector3d posVec = a.col(i);
+        Eigen::Vector3d velVec = b.col(i);
+
+        // Compute the cross product and store it in the result matrix
+        x.col(i) = posVec.cross(velVec);
+    }
+
+    return x;
+}
+
+void nadir_frame(const Eigen::MatrixXd &position, const Eigen::MatrixXd &velocity)
+{
+    Eigen::MatrixXd angular_momentum = cross(position, velocity);
+
+    // z = -position ./ vecnorm(position);
+    Eigen::MatrixXd z = -(position.array().rowwise() / position.colwise().norm().array());
+    // y = -angular_momentum ./ vecnorm(angular_momentum);
+    Eigen::MatrixXd y = -(angular_momentum.array().rowwise() / angular_momentum.colwise().norm().array());
+    Eigen::MatrixXd x = cross(y, z);
+
+    std::cout << x.col(0) << std::endl;
+}
+
 int main()
 {
 
@@ -44,6 +82,8 @@ int main()
             m_i_v.col(i) = i_v;
         }
 
+        nadir_frame(m_i_r, m_i_v);
+
         // Save to file
         auto ts = DateTime::getCurrentTimestamp();
         DB::writematrix(m_i_r, "./output/" + ts, "i_r.csv");
@@ -51,21 +91,6 @@ int main()
         DB::serializeTimePointsToCSV(date_times, "./output/" + ts, "t.csv");
 
         std::cout << "Data saved to output.txt" << std::endl;
-
-        // MatrixXd A(2, 3);
-        // A << 1, 2, 3,
-        //     4, 5, 6;
-
-        // MatrixXd B(3, 2);
-        // B << 7, 8,
-        //     9, 10,
-        //     11, 12;
-
-        // MatrixXd result = A * B;
-        // MatrixXd transposedResult = result.transpose();
-
-        // cout << "Result of (A * B)^T:\n"
-        //      << transposedResult << endl;
     }
     catch (const exception &e)
     {
