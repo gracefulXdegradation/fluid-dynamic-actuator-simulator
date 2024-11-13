d5d92253dafb75a4fce88d245034f36a5816262f
Sasha Grynevych <alexandr.grinevich@gmail.com>
2024-11-13 20:42:08 +0100

    feat: added boost for ode

--- simulator/src/main.cpp
+++ simulator/src/main.cpp
@@ -8,9 +8,11 @@
 #include "OrbitalMechanics.h"
 #include "DB.hpp"
 #include <cmath>
+#include <boost/numeric/odeint.hpp>
 
 using namespace Eigen;
 using namespace std;
+namespace odeint = boost::numeric::odeint;
 
 // Checks whether combined contact criterion of access == 1 (satellite is
 // inside cone drawn by ground station FOV) and distance < 1.5e6 m
@@ -141,8 +143,40 @@ std::pair<Vector3d, Vector3d> TetrahedronActuatorAssemblyPropertyExtractor(
     return std::make_pair(b_angular_momentum, b_torque);
 }
 
+typedef std::array<double, 2> state_type;
+
+struct pendulum
+{
+    double m_mu, m_omega, m_eps;
+    pendulum(double mu, double omega, double eps)
+        : m_mu(mu), m_omega(omega), m_eps(eps) {}
+    void operator()(const state_type &x,
+                    state_type &dxdt, double t) const
+    {
+        dxdt[0] = x[1];
+        dxdt[1] = -sin(x[0]) - m_mu * x[1] +
+                  m_eps * sin(m_omega * t);
+    }
+};
+
 int main()
 {
+    odeint::runge_kutta4<state_type> rk4;
+    pendulum p(0.1, 1.05, 1.5);
+    state_type x = {{1.0, 0.0}};
+    double t = 0.0;
+    const double dt = 0.01;
+    rk4.do_step(p, x, t, dt);
+    t += dt;
+
+    std::cout << t << " " << x[0] << " " << x[1] << "\n";
+    for (size_t i = 0; i < 10; ++i)
+    {
+        rk4.do_step(p, x, t, dt);
+        t += dt;
+        std::cout << t << " " << x[0] << " " << x[1] << "\n";
+    }
+
     // Create an instance of ConfigParser with the path to your config.json
     Config config(string(BUILD_OUTPUT_PATH) + "/config.json");
 
